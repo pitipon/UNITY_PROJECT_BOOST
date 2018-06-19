ba0181bf97465f7212fdd964e4d177a50c8fa8a5
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-06-19 23:02:05 +0800

    rocket can control boost by space .. and Rotate by pressing A or D

---
+++ Assets/control.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInput();
+	}
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+            print("Rocket Go up");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
